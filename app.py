5a489fd951f0286822e816fb198756369e1b9d66
sharmila2905-collab <sharmilas2905@gmail.com>
2024-06-26 14:00:58 +0530

    first push

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import os
 
-st.write("Hello World")
-st.write("## This is a H2 Title!")
-x = st.text_input("Movie", "Star Wars")
+file_path = "/mount/src/hello/movies.csv"
 
-if st.button("Click Me"):
-    st.write(f"Your favorite movie is {x}")
-
-
-data = pd.read_csv("movies.csv")
-st.write(data)
-
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-st.bar_chart(chart_data)
+try:
+    data = pd.read_csv(file_path)
+    print("File read successfully.")
+except Exception as e:
+    print(f"Error reading the file: {e}")
